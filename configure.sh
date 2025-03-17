4bea3620e93f910ad20a20698c5f4beda63230eb
Aarush Luthra <163556661+Prachand07@users.noreply.github.com>
2025-03-17 22:34:11 +0530

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -3,6 +3,8 @@ sudo yum update -y
 sudo yum install -y httpd
 sudo systemctl start httpd
 sudo systemctl enable httpd
+cd /var/www/html/Frontend
+sudo echo "CONFIG = { PUBLIC_IP: '$(curl -s ifconfig.me)' };" > config.js
 cd /var/www/html/backend
 REPO_FILE="/etc/yum.repos.d/mongodb-org-8.0.repo"
 echo "[mongodb-org-8.0]
@@ -15,7 +17,6 @@ sudo yum install -y mongodb-org
 sudo systemctl start mongod
 sudo systemctl enable mongod
 aws ssm get-parameters --names "MONGO_CLUSTER" "MONGO_DB" "MONGO_OPTIONS" "MONGO_PASSWORD" "MONGO_USER" "PORT" --with-decryption --query "Parameters[*].{Name:Name,Value:Value}" --output text > .env
-sudo echo "CONFIG = { PUBLIC_IP: '$(curl -s ifconfig.me)' };" > config.js
 sudo yum install -y nodejs
 sudo npm install -g pm2
 sudo pm2 start index.js
